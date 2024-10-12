c73553c39d0a3a433738f68d5f9b570aea2f67b6
gudokk <masagudkova0@gmail.com>
2024-10-12 19:49:33 +0700

    Initial Commit

---
+++ third_lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  third_lesson
+//
+//  Created by Маша Гудкова on 12.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
